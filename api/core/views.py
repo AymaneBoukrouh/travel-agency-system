c56c051bfcfe866d20d542272384d5010ada1475
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-20 02:27:42 +0100

    add multi-language support

--- api/core/views.py
+++ api/core/views.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
+from django.utils.translation import gettext as _
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def hello_world(request):
-    return Response({'message': 'Hello, World!'})
+    return Response({'message': _('Hello')})
--- web/src/App.js
+++ web/src/App.js
@@ -4,18 +4,44 @@ import './App.css';
 
 function App() {
   const [message, setMessage] = useState('Loading...');
-  useEffect(() => {
-    fetch('http://localhost:8000/api/hello_world')
+
+  const updateMessage = (locale) => {
+    fetch('http://localhost:8000/api/hello_world/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept-Language': locale
+      }
+    })
       .then(response => response.json())
       .then(data => {
         setMessage(data.message);
       });
+  }
+
+  useEffect(() => {
+    updateMessage('en-EN');
   }, []);
+
+  const updateLocale = () => {
+    updateMessage(document.getElementById('locale').value);
+  }
+
+  const locales = [
+    { code: 'en-US', name: 'English' },
+    { code: 'fr-FR', name: 'French' },
+    { code: 'de-DE', name: 'German' }
+  ];
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <select onChange={() => updateLocale()} id="locale">
+          {locales.map(locale => (
+            <option key={locale.code} value={locale.code}>{locale.name}</option>
+          ))}
+        </select>
         <p>
           { message }
         </p>
