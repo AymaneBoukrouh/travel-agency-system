3266cfae6d11794ec5c129f13a649ba4938a20ee
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-24 01:01:27 +0100

    add views.register basic test

--- api/core/tests.py
+++ api/core/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import User
 
-# Create your tests here.
+
+class CoreApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_register(self):
+        # set payload
+        payload = {
+            'firstname': 'John',
+            'lastname': 'Doe',
+            'email': 'johndoe@email.com',
+            'password': 'password'
+        }
+
+        # check database is empty
+        self.assertEqual(User.objects.count(), 0)
+
+        # make request
+        response = self.client.post('/api/register', payload)
+
+        # check response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), payload)
+
+        # check user added to database
+        self.assertTrue(User.objects.filter(email=payload['email']).exists())
