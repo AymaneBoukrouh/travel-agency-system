45b90d34bc44875144efa8129f1208d36d7d5571
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-25 00:44:58 +0100

    hash password on register

--- api/core/views.py
+++ api/core/views.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from core.models import User
+import bcrypt
 
 @api_view(['GET'])
 def hello_world(request):
@@ -11,12 +12,16 @@ def hello_world(request):
 
 @api_view(['POST'])
 def register(request):
+    # hash password
+    hashed_password = bcrypt.hashpw(request.data['password'].encode('utf-8'), bcrypt.gensalt())
+
+    # create user if email is unique
     try:
         user = User.objects.create(
             firstname = request.data['firstname'],
             lastname = request.data['lastname'],
             email = request.data['email'],
-            password = request.data['password'] # TODO: encrypt password
+            password = hashed_password.decode('utf-8')
         )
     
     except db.utils.IntegrityError:
