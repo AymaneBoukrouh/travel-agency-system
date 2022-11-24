bd11cc27ded230cd7a81f9452dff8d5c46744d4d
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-24 01:01:03 +0100

    add views.register

--- api/core/urls.py
+++ api/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('hello_world/', views.hello_world)
+    path('hello_world', views.hello_world),
+    path('register', views.register)
 ]
--- api/core/views.py
+++ api/core/views.py
@@ -2,7 +2,29 @@ from django.http import JsonResponse
 from django.utils.translation import gettext as _
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from core.models import User
 
 @api_view(['GET'])
 def hello_world(request):
     return Response({'message': _('Hello')})
+
+@api_view(['POST'])
+def register(request):
+    # check if email already exists
+    if User.objects.filter(email=request.data['email']).exists():
+        return Response({'message': _('An account with this email already exists.')}, status=400)
+
+    # create user
+    user = User.objects.create(
+        firstname = request.data['firstname'],
+        lastname = request.data['lastname'],
+        email = request.data['email'],
+        password = request.data['password'] # TODO: encrypt password
+    )
+
+    # add user to database
+    user.save()
+    print(user.serialize())
+
+    # return user
+    return Response(request.data)
