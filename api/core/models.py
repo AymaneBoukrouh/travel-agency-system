98aa571d9da312f18250ef3ebcb975596255daf9
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-24 01:15:44 +0100

    fix: make email field unique

--- api/core/models.py
+++ api/core/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     firstname = models.CharField(max_length=64)
     lastname = models.CharField(max_length=64)
-    email = models.EmailField(max_length=64)
+    email = models.EmailField(max_length=64, unique=True)
     password = models.CharField(max_length=60) # 60 is the max length of a bcrypt hash
     created_at = models.DateTimeField(auto_now_add=True)
 
