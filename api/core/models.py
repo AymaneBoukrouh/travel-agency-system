03b617e963e21aaa3c83cf5753ce986dfee137a3
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-24 01:00:12 +0100

    add User model

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64)
+    password = models.CharField(max_length=60) # 60 is the max length of a bcrypt hash
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'email': self.email,
+            'created_at': self.created_at.strftime('%b %d %Y, %I:%M %p')
+        }
