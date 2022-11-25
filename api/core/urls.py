f5fa1e0989c264566430cdfe18d831f53aba3caf
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-25 02:34:34 +0100

    implement jwt auth

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from core import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('hello_world', views.hello_world),
-    path('register', views.register)
+    path('register', views.register),
+    path('login', TokenObtainPairView.as_view()),
+    path('refresh', TokenRefreshView.as_view())
 ]
