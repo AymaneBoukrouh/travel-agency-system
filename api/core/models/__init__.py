67b846e60bb953d5d782e960d9f8e582071a2034
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-27 23:13:06 +0100

    fix wrong import

--- api/core/models/__init__.py
+++ api/core/models/__init__.py
@@ -1,5 +1,4 @@
 from core.models.hotel import Hotel
 from core.models.office import Office
 from core.models.reservation import Reservation
-from core.models.travel import Travel
-from core.models.transport import Transport
+from core.models.trip import Trip
