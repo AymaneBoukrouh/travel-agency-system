aa63c011f1489c6928ea6193c1dfce2c92f3bcf7
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-29 00:09:23 +0100

    save locale to localStorage and use mui components

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,16 @@ import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { TextField, MenuItem } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   const [message, setMessage] = useState('Loading...');
 
   const updateMessage = (locale) => {
@@ -19,34 +28,46 @@ function App() {
       });
   }
 
+  const locale = localStorage.getItem('locale') || 'en-US';
   useEffect(() => {
-    updateMessage('en-EN');
+    updateMessage(locale);
   }, []);
 
-  const updateLocale = () => {
-    updateMessage(document.getElementById('locale').value);
-  }
-
+  // locales: en-EN, de-DE, fr-FR
   const locales = [
     { code: 'en-US', name: 'English' },
     { code: 'fr-FR', name: 'French' },
     { code: 'de-DE', name: 'German' }
   ];
 
+  const handleUpdateLocale = (event) => {
+    updateMessage(event.target.value);
+    localStorage.setItem('locale', event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <select onChange={() => updateLocale()} id="locale">
-          {locales.map(locale => (
-            <option key={locale.code} value={locale.code}>{locale.name}</option>
-          ))}
-        </select>
-        <p>
-          { message }
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <TextField
+              id = 'locale'
+              defaultValue = {locale}
+              onChange = {handleUpdateLocale}
+              select
+            >
+              {locales.map(locale => (
+                <MenuItem key={locale.code} value={locale.code}>
+                  {locale.name}
+                </MenuItem>
+              ))}
+            </TextField>
+          <p>
+            { message }
+          </p>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
