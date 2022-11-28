e128433f4e9877ff1054177746b8f9c9ccad0c9c
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-28 01:11:20 +0100

    fix ci pipeline

--- api/test.sh
+++ api/test.sh
@@ -3,11 +3,12 @@
 # build image
 docker compose -f docker-compose.yml -f docker-compose.test.yml build api
 
-# run container
-docker compose -f docker-compose.yml -f docker-compose.test.yml up api -d
-
-# run tests
-docker compose -f docker-compose.yml -f docker-compose.test.yml run api python manage.py test
+# run container and get exit code
+docker compose -f docker-compose.yml -f docker-compose.test.yml up api --exit-code-from api --abort-on-container-exit
+exit_code=$?
 
 # stop container
-docker compose down
+docker compose -f docker-compose.yml -f docker-compose.test.yml down
+
+# exit with container exit code
+exit $exit_code
