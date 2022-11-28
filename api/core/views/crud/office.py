fc9f3e0fcdaf5667ad365f2e5c2f3d16ee024dbf
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-28 02:20:51 +0100

    switch from APIView to mixins and generics

--- api/core/views/crud/office.py
+++ api/core/views/crud/office.py
@@ -1,45 +1,28 @@
 from core.models import Office
 from core.serializers import OfficeSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins, generics
 
-# TODO: switch to mixins and generics
 
-class OfficeList(APIView):
+class OfficeList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Office.objects.all()
+    serializer_class = OfficeSerializer
+
     def get(self, request):
-        offices = Office.objects.all()
-        serializer = OfficeSerializer(offices, many=True)
-        return Response(serializer.data)
+        return self.list(request)
 
     def post(self, request):
-        serializer = OfficeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
 
-class OfficeDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Office.objects.get(pk=pk)
-        except Office.DoesNotExist:
-            raise Http404
+
+class OfficeDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Office.objects.all()
+    serializer_class = OfficeSerializer
 
     def get(self, request, pk):
-        office = self.get_object(pk)
-        serializer = OfficeSerializer(office)
-        return Response(serializer.data)
+        return self.retrieve(request, pk)
 
     def put(self, request, pk):
-        office = self.get_object(pk)
-        serializer = OfficeSerializer(office, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.update(request, pk)
 
     def delete(self, request, pk):
-        office = self.get_object(pk)
-        office.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return self.destroy(request, pk)
