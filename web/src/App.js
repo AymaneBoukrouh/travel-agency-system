78df76fec4153505736b6dcb7a322d9cce73de49
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-28 23:57:37 +0100

    remove startup react code

--- web/src/App.js
+++ web/src/App.js
@@ -45,14 +45,6 @@ function App() {
         <p>
           { message }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
