7d47aa39a2cbe1a606180d7e0f446350b60eca3d
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-29 01:59:48 +0100

    move hello_world example to dedicated views directory

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { TextField, MenuItem } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+import HelloWorld from './views/HelloWorld';
 
 function App() {
   // theme
@@ -13,60 +13,12 @@ function App() {
     },
   });
 
-  // locale
-  const locales = [
-    { code: 'en-US', name: 'English' },
-    { code: 'fr-FR', name: 'Français' },
-    { code: 'de-DE', name: 'Deutsch' }
-  ];
-
-  const [locale, setLocale] = useState(localStorage.getItem('locale') || 'en-US');
-
-  const handleLocaleChange = (event: any) => {
-    setLocale(event.target.value);
-    localStorage.setItem('locale', event.target.value);
-  }
-
-  // hello world
-  const [helloWorld, setHelloWorld] = useState('Loading...');
-  const updateHelloWorld = () => {
-    fetch('http://localhost:8000/api/hello_world', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept-Language': locale
-      }
-    })
-      .then(response => response.json())
-      .then(data => setHelloWorld(data.message));
-  };
-
-  // init
-  useEffect(() => {
-    updateHelloWorld();
-  }, [locale]);
-
   return (
     <ThemeProvider theme={darkTheme}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <TextField
-            id = 'locale'
-            select
-            label = 'Locale'
-            value = {locale}
-            onChange = {handleLocaleChange}
-          >
-            {locales.map((option) => (
-              <MenuItem key={option.code} value={option.code}>
-                {option.name}
-              </MenuItem>
-            ))}
-          </TextField>
-          <div className="p-3">
-            {helloWorld}
-          </div>
+          <HelloWorld />
         </header>
       </div>
     </ThemeProvider>
---
+++ web/src/views/HelloWorld.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+
+import { TextField, MenuItem } from '@mui/material';
+
+const HelloWorld = () => {
+  // locale
+  const locales = [
+    { code: 'en-US', name: 'English' },
+    { code: 'fr-FR', name: 'Français' },
+    { code: 'de-DE', name: 'Deutsch' }
+  ];
+
+  const [locale, setLocale] = useState(localStorage.getItem('locale') || 'en-US');
+
+  const handleLocaleChange = (event: any) => {
+    setLocale(event.target.value);
+    localStorage.setItem('locale', event.target.value);
+  }
+
+  // hello world
+  const [helloWorld, setHelloWorld] = useState('Loading...');
+  const updateHelloWorld = () => {
+    fetch('http://localhost:8000/api/hello_world', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept-Language': locale
+      }
+    })
+      .then(response => response.json())
+      .then(data => setHelloWorld(data.message));
+  };
+
+  // init
+  useEffect(() => {
+    updateHelloWorld();
+  }, [locale]);
+
+  return (
+    <div>
+      <TextField
+        id = 'locale'
+        select
+        label = 'Locale'
+        value = {locale}
+        onChange = {handleLocaleChange}
+      >
+        {locales.map((option) => (
+          <MenuItem key={option.code} value={option.code}>
+            {option.name}
+          </MenuItem>
+        ))}
+      </TextField>
+      <div className="p-3">
+        {helloWorld}
+      </div>
+    </div>
+  )
+}
+
+export default HelloWorld;
