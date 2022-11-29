7bd23c0c46cbe01aa607effa10353b06ffb9b696
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-29 01:14:46 +0100

    rewrite hello_world example using TypeScript

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { TextField, MenuItem } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 function App() {
+  // theme
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark'
+    },
+  });
+
+  // locale
+  const locales = [
+    { code: 'en-US', name: 'English' },
+    { code: 'fr-FR', name: 'Français' },
+    { code: 'de-DE', name: 'Deutsch' }
+  ];
+
+  const [locale, setLocale] = useState(localStorage.getItem('locale') || 'en-US');
+
+  const handleLocaleChange = (event: any) => {
+    setLocale(event.target.value);
+    localStorage.setItem('locale', event.target.value);
+  }
+
+  // hello world
+  const [helloWorld, setHelloWorld] = useState('Loading...');
+  const updateHelloWorld = () => {
+    fetch('http://localhost:8000/api/hello_world', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept-Language': locale
+      }
+    })
+      .then(response => response.json())
+      .then(data => setHelloWorld(data.message));
+  };
+
+  // init
+  useEffect(() => {
+    updateHelloWorld();
+  }, [locale]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <TextField
+            id = 'locale'
+            select
+            label = 'Locale'
+            value = {locale}
+            onChange = {handleLocaleChange}
+          >
+            {locales.map((option) => (
+              <MenuItem key={option.code} value={option.code}>
+                {option.name}
+              </MenuItem>
+            ))}
+          </TextField>
+          <div className="p-3">
+            {helloWorld}
+          </div>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
