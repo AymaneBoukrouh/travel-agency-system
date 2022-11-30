6f01a0b25d0f4c51301a097f801afb83b30c2b0e
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-11-30 01:27:00 +0100

    make TopBar visible

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <div className="h-100">
-        {/*<TopBar />*/}
+        <TopBar />
         <Outlet />
       </div>
     </ThemeProvider>
