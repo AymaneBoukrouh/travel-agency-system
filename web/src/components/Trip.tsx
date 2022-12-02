be8e76c8d501c3a86c6e112141680db3328fa794
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-02 19:34:00 +0100

    rename class from trip-img-wrapper-box to trip-img-box

--- web/src/components/Trip.tsx
+++ web/src/components/Trip.tsx
@@ -11,7 +11,7 @@ const Trip = (trip: TripProps) => {
     <div className="trip-wrapper">
       <div className="trip-img-wrapper position-relative rounded-3 mb-3 w-100 ratio ratio-1x1">
         <img className="trip-img img-fluid ratio ratio-1x1" src={trip.image} alt="Trip" />
-        <div className="trip-img-wrapper-box d-flex justify-content-center align-items-center position-absolute top-100 w-100 h-100 rounded-3">
+        <div className="trip-img-box d-flex justify-content-center align-items-center position-absolute top-100 w-100 h-100 rounded-3">
           <div className="h2">
             {trip.name}
           </div>
