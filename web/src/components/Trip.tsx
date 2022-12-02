289ff95441d4adfc3c86e0089cb4507c38fce20b
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-02 15:32:12 +0100

    add hoverable box on trip

--- web/src/components/Trip.tsx
+++ web/src/components/Trip.tsx
@@ -8,9 +8,14 @@ interface TripProps {
 
 const Trip = (trip: TripProps) => {
   return (
-    <div>
-      <div className="trip-img-wrapper rounded-3 mb-3 w-100 ratio ratio-1x1">
+    <div className="trip-wrapper">
+      <div className="trip-img-wrapper position-relative rounded-3 mb-3 w-100 ratio ratio-1x1">
         <img className="trip-img img-fluid ratio ratio-1x1" src={trip.image} alt="Trip" />
+        <div className="trip-img-wrapper-box d-flex justify-content-center align-items-center position-absolute top-100 w-100 h-100 rounded-3">
+          <div className="h2">
+            {trip.name}
+          </div>
+        </div>
       </div>
       <h3>{trip.name}</h3>
       <div>Starting from $430</div>
