12d880fe6b1c371b68057d675892500bdf72c349
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-04 23:37:28 +0100

    add global using statements

---
+++ microservices/hotel/GlobalUsing.cs
@@ -0,0 +1,5 @@
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.EntityFrameworkCore;
+global using hotel.Models;
+global using hotel.DAL;
