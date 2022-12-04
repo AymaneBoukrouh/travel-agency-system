a3dd0efc7d970cb4c4cff9671fe31d8d8f505dbe
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-04 23:36:40 +0100

    configure mysql connection string

--- microservices/hotel/Program.cs
+++ microservices/hotel/Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+{
+  options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
