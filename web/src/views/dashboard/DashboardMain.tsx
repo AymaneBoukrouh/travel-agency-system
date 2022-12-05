2164f2d0731a98d266404ab53048b95a66fb7884
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-05 14:11:14 +0100

    add charts to dashboard

--- web/src/views/dashboard/DashboardMain.tsx
+++ web/src/views/dashboard/DashboardMain.tsx
@@ -1,6 +1,48 @@
+import DashboardExpenses from '../../components/DashboardExpenses';
+import DashboardStatItem from '../../components/DashboardStatItem';
+import DashboardVisits from '../../components/DashboardVisits';
+
+import { Card } from '@mui/material';
+import { People, Visibility, Tour, Apartment } from '@mui/icons-material';
+
 const DashboardMain = () => {
+
   return (
-    <div></div>
+    <div className="container-fluid">
+      <div className="row g-3">
+        <div className="col-2">
+          <DashboardStatItem value={520} name="Users" comment="in total" Icon={People} iconColor="orange" />
+        </div>
+        <div className="col-2">
+          <DashboardStatItem value={422} name="Visits" comment="this month" Icon={Visibility} iconColor="gray" />
+        </div>
+        <div className="col-2">
+          <DashboardStatItem value={27} name="Trips" comment="in total" Icon={Tour} iconColor="red"  />
+        </div>
+        <div className="col-2">
+          <Card className="text-dark px-3 py-2 bg-white" elevation={0}>
+            <h1 className="fw-bold mb-0">3</h1>
+            <div>Active</div>
+          </Card>
+        </div>
+        <div className="col-2">
+          <DashboardStatItem value={42} name="Completed" comment="this month" Icon={Tour} iconColor="green" />
+        </div>
+        <div className="col-2">
+          <DashboardStatItem value={3} name="Offices" comment="in total" Icon={Apartment} iconColor="blue" />
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <Card className="p-3 bg-white text-dark" elevation={0}>
+            <DashboardExpenses />
+          </Card>
+        </div>
+        <div className="col-12 col-md-6 col-lg-6">
+          <Card className="p-3 bg-white text-dark h-100" elevation={0}>
+            <DashboardVisits />
+          </Card>
+        </div>
+      </div>
+    </div>
   )
 }
 
