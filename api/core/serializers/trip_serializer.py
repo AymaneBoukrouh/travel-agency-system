a7136e470fb3cde40eeebd41f149dc1244b716aa
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-06 23:30:52 +0100

    add number of nights to trip serializer

--- api/core/serializers/trip_serializer.py
+++ api/core/serializers/trip_serializer.py
@@ -4,4 +4,4 @@ from core.models import Trip
 class TripSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
-        fields = ['id', 'departure_date', 'arrival_date', 'destination', 'price', 'transport_mode']
+        fields = ['id', 'departure_date', 'arrival_date', 'destination', 'price', 'transport_mode', 'office_id', 'nights']
