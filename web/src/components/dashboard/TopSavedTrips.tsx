bbeda727ead061019c817dca90243f95dc012b9b
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-06 10:59:23 +0100

    add TopSavedTrips component (empty)

---
+++ web/src/components/dashboard/TopSavedTrips.tsx
@@ -0,0 +1,9 @@
+const TopSavedTrips = () => {
+  return (
+    <div>
+      <h3 className="mb-3">Top Saved Trips</h3>
+    </div>
+  )
+}
+
+export default TopSavedTrips;
--- web/src/views/dashboard/Main.tsx
+++ web/src/views/dashboard/Main.tsx
@@ -1,9 +1,10 @@
 import { Card } from '@mui/material';
-import { People, Visibility, Tour, Apartment } from '@mui/icons-material';
+import { People, Visibility, Tour, Apartment, Bookmark } from '@mui/icons-material';
 
 import DashboardExpenses from '@/components/dashboard/ExpensesChart';
 import DashboardVisits from '@/components/dashboard/VisitsChart';
-import DashboardStatItem from '@/components/dashboard/StatCard';
+import DashboardStatCard from '@/components/dashboard/StatCard';
+import DashboardTopSavedTrips from '@/components/dashboard/TopSavedTrips';
 
 const Main = () => {
 
@@ -11,25 +12,22 @@ const Main = () => {
     <div className="container-fluid">
       <div className="row g-3">
         <div className="col-2">
-          <DashboardStatItem value={520} name="Users" comment="in total" Icon={People} iconColor="orange" />
+          <DashboardStatCard value={27} name="Trips" comment="in total" Icon={Tour} iconColor="red"  />
         </div>
         <div className="col-2">
-          <DashboardStatItem value={422} name="Visits" comment="this month" Icon={Visibility} iconColor="gray" />
+          <DashboardStatCard value={573} name="Bookings" comment="in total" Icon={Bookmark} iconColor="blue" />
         </div>
         <div className="col-2">
-          <DashboardStatItem value={27} name="Trips" comment="in total" Icon={Tour} iconColor="red"  />
+          <DashboardStatCard value={520} name="Users" comment="in total" Icon={People} iconColor="orange" />
         </div>
         <div className="col-2">
-          <Card className="text-dark px-3 py-2 bg-white" elevation={0}>
-            <h1 className="fw-bold mb-0">3</h1>
-            <div>Active</div>
-          </Card>
+          <DashboardStatCard value={422} name="Visits" comment="this month" Icon={Visibility} iconColor="gray" />
         </div>
         <div className="col-2">
-          <DashboardStatItem value={42} name="Completed" comment="this month" Icon={Tour} iconColor="green" />
+          <DashboardStatCard value={42} name="Completed" comment="this month" Icon={Tour} iconColor="green" />
         </div>
         <div className="col-2">
-          <DashboardStatItem value={3} name="Offices" comment="in total" Icon={Apartment} iconColor="blue" />
+          <DashboardStatCard value={3} name="Offices" comment="in total" Icon={Apartment} iconColor="blue" />
         </div>
         <div className="col-12 col-md-6 col-lg-3">
           <Card className="p-3 bg-white text-dark" elevation={0}>
@@ -39,6 +37,11 @@ const Main = () => {
         <div className="col-12 col-md-6 col-lg-6">
           <Card className="p-3 bg-white text-dark h-100" elevation={0}>
             <DashboardVisits />
+          </Card>
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <Card className="p-3 bg-white text-dark h-100" elevation={0}>
+            <DashboardTopSavedTrips />
           </Card>
         </div>
       </div>
