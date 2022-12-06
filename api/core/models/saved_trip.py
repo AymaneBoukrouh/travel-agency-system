8c5fc691c648814b9ac48ba75c1e9d9c94aea6e7
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-06 22:40:36 +0100

    change SavedTrip foreign key to django auth model

--- api/core/models/saved_trip.py
+++ api/core/models/saved_trip.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.db import models
 
 class SavedTrip(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
