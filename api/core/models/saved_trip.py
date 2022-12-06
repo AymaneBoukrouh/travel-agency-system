c1546430713631be9b1d937b51dbc3c9a4392170
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-06 22:38:40 +0100

    fix typo

--- api/core/models/saved_trip.py
+++ api/core/models/saved_trip.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class SavedeTrip(models.Model):
+class SavedTrip(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
