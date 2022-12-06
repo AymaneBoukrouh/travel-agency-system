c70509ef5e98f9dab842a911a16b26558c24d8a9
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-06 22:27:47 +0100

    add saved_trip model

--- api/core/models/__init__.py
+++ api/core/models/__init__.py
@@ -1,4 +1,5 @@
 from core.models.hotel import Hotel
 from core.models.office import Office
 from core.models.reservation import Reservation
+from core.models.saved_trip import SavedTrip
 from core.models.trip import Trip
