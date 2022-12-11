572ba7840df40f62c8dda32c2741aaa67466c9f0
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-11 07:57:18 +0100

    add register hotel microservice in the gateway

--- microservices/api.gateway/Program.cs
+++ microservices/api.gateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+await app.UseOcelot();
 
 app.UseAuthorization();
 
