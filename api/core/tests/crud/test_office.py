8dd75736235925981c64ee2ecdfb10756b75090f
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-17 23:53:42 +0100

    write better office view tests

--- api/core/tests/crud/test_office.py
+++ api/core/tests/crud/test_office.py
@@ -1,42 +1,55 @@
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient
 from core.models import Office
 
-class OfficeCRUDTests(TestCase):
+class OfficeViewTestCase(TestCase):
     def setUp(self):
-        self.client = APIClient()
+        self.office_data = {'city': 'New York', 'zipcode': 10001}
+        self.office = Office.objects.create(**self.office_data)
+        self.url = reverse('office-list')
 
-    def test_create_office(self):
-        '''
-        Tests:
-        1. create office
-        2. re-create office
-        '''
+    def test_office_list_view(self):
+        # Test GET request to OfficeList view
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['city'], self.office_data['city'])
+        self.assertEqual(response.data[0]['zipcode'], self.office_data['zipcode'])
 
-        # 1. create office
-        # set payload
-        payload = {
-            'city': 'New York',
-            'zipcode': '90000'
-        }
+        # Test POST request to OfficeList view
+        new_office_data = {'city': 'Los Angeles', 'zipcode': 90001}
+        response = self.client.post(self.url, data=new_office_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['city'], new_office_data['city'])
+        self.assertEqual(response.data['zipcode'], new_office_data['zipcode'])
 
-        # check database is empty
+    def test_office_detail_view(self):
+        # Test GET request to OfficeDetail view
+        response = self.client.get(self.url + f'/{self.office.pk}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['city'], self.office_data['city'])
+        self.assertEqual(response.data['zipcode'], self.office_data['zipcode'])
+
+        # Test PUT request to OfficeDetail view
+        new_data = {'city': 'Chicago', 'zipcode': 60601}
+        response = self.client.put(self.url + f'/{self.office.pk}', data=new_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['city'], new_data['city'])
+        self.assertEqual(response.data['zipcode'], new_data['zipcode'])
+
+        # Test DELETE request to OfficeDetail view
+        response = self.client.delete(self.url + f'/{self.office.pk}')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Office.objects.count(), 0)
 
-        # make request
-        response = self.client.post('/api/offices', payload)
+    def test_office_detail_view_invalid_pk(self):
+        # Test GET request to OfficeDetail view with invalid pk
+        invalid_pk_url = self.url + '/999'
+        response = self.client.get(invalid_pk_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-        # check response
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.json()['city'], payload['city'])
-
-        # check office added to database
-        self.assertTrue(Office.objects.filter(city=payload['city']).exists())
-
-        # 2. re-create office
-        # make request
-        response = self.client.post('/api/offices', payload)
-
-        # check response
+    def test_office_list_view_invalid_data(self):
+        # Test POST request to OfficeList view with invalid data
+        invalid_data = {'city': '', 'zipcode': 90001}
+        response = self.client.post(self.url, data=invalid_data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
