5386d6d689e9be11c4fa7558a1b6ce110865c384
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-24 18:47:58 +0100

    add users table with dummy data

--- web/src/views/dashboard/Users.tsx
+++ web/src/views/dashboard/Users.tsx
@@ -1,6 +1,17 @@
+import DashboardTable from '@/components/dashboard/Table';
+
 const Users = () => {
   return (
-    <div></div>
+    <div className="container-fluid bg-white p-5 rounded-3">
+      <h3 className="mb-4">Users</h3>
+      <DashboardTable
+        columns = {['firstname', 'lastname', 'email']}
+        column_names = {['First Name', 'Last Name', 'E-Mail']}
+        data = {[
+          {firstname: 'John', lastname: 'Doe', email: 'dd'}
+        ]}
+      />
+    </div>
   )
 }
 
