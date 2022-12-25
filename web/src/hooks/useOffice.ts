fddba7f8d0f2dd0e209aef8cbd46808e82c12266
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-25 15:47:46 +0100

    refactor: create checkResponse for reusability

--- web/src/hooks/useOffice.ts
+++ web/src/hooks/useOffice.ts
@@ -2,34 +2,36 @@ import { useState } from 'react';
 import { Office } from '@/types/Office';
 
 
+const checkResponse = async (response: Response, setError: callable) => {
+  const data = await response.json();
+
+  if (!response.ok) {
+    setError(data.message);
+    return false;
+  }
+
+  return data;
+}
+
+
 export const useOffice = () => {
   const [error, setError] = useState('');
-
-  // TODO: refactor: create return error function
 
   // GET /api/offices
   const getOffices = async () => {
     const response = await fetch('http://localhost:8000/api/offices');
-    const data = await response.json();
+    const data = await checkResponse(response, setError);
 
-    if (!response.ok) {
-      setError(data.message);
-      return;
-    }
-
+    if (!data) return;
     return data as Office[];
   }
 
   // GET /api/offices/:id
   const getOffice = async (id: number) => {
     const response = await fetch(`http://localhost:8000/api/offices/${id}`);
-    const data = await response.json();
+    const data = await checkResponse(response, setError);
 
-    if (!response.ok) {
-      setError(data.message);
-      return;
-    }
-
+    if (!data) return;
     return data as Office;
   }
 
@@ -43,13 +45,9 @@ export const useOffice = () => {
       body: JSON.stringify(office),
     });
 
-    const data = await response.json();
+    const data = await checkResponse(response, setError);
 
-    if (!response.ok) {
-      setError(data.message);
-      return;
-    }
-
+    if (!data) return;
     return data as Office;
   }
 
@@ -63,13 +61,9 @@ export const useOffice = () => {
       body: JSON.stringify(office),
     });
 
-    const data = await response.json();
+    const data = await checkResponse(response, setError);
 
-    if (!response.ok) {
-      setError(data.message);
-      return;
-    }
-
+    if (!data) return;
     return data as Office;
   }
 
@@ -79,7 +73,7 @@ export const useOffice = () => {
       method: 'DELETE',
     });
 
-    if (!response.ok) {
+    if (!response.ok) { // not using checkResponse here because we don't need the data
       setError('An error occured.');
       return false;
     }
