31f66727e316705e22d4cff45c773c25e094a8f0
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-25 15:32:50 +0100

    add user serializer

--- api/core/serializers/__init__.py
+++ api/core/serializers/__init__.py
@@ -4,3 +4,4 @@ from core.serializers.reservation_serializer import ReservationSerializer
 from core.serializers.saved_trip_serializer import SavedTripSerializer
 from core.serializers.trip_click_serializer import TripClickSerializer
 from core.serializers.trip_serializer import TripSerializer
+from core.serializers.user_serializer import UserSerializer
