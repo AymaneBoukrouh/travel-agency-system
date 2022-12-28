ed1a2a2d187ad1535aa37030f726dc14d0845386
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-28 05:50:13 +0100

    add user_id and trip_id to booking_serializer

--- api/core/serializers/booking_serializer.py
+++ api/core/serializers/booking_serializer.py
@@ -4,4 +4,4 @@ from core.models import Booking
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ['id', 'number_of_people']
+        fields = ['id', 'user_id', 'trip_id', 'number_of_people']
