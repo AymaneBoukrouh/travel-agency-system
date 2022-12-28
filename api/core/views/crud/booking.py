cfaa4a103dee7a12fe5aa0c3d3f8f86269d270d8
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-28 06:11:03 +0100

    validate number of people when booking

--- api/core/views/crud/booking.py
+++ api/core/views/crud/booking.py
@@ -1,5 +1,7 @@
-from core.models import Booking
+from core.models import Booking, Trip
 from core.serializers import BookingSerializer
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 from rest_framework import mixins, generics
 
 
@@ -11,9 +13,16 @@ class BookingList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Gener
         return self.list(request)
 
     def post(self, request):
+        if request.data['number_of_people'] <= 0:
+            return JsonResponse({'error': 'Number of people must be greater than 0'}, status=400)
+
         return self.create(request)
 
-
+    def perform_create(self, serializer):
+        trip = Trip.objects.get(id=self.request.data['trip_id'])
+        user = User.objects.get(id=self.request.data['user_id'])
+        serializer.save(trip=trip, user=user)
+  
 class BookingDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
