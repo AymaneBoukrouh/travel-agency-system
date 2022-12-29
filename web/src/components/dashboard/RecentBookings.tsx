cc14e50ce9d3ca3e77c4b2ed3e628e4bf2b434d7
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-12-29 05:59:24 +0100

    add RecentBookings component

---
+++ web/src/components/dashboard/RecentBookings.tsx
@@ -0,0 +1,121 @@
+import { useRef, useEffect } from 'react';
+
+const RecentBookings = () => {
+  const bookings = [
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Paid',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Paid',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Paid',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Cancelled',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Paid',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Paid',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Pending',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Paid',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Paid',
+      price: 100
+    },
+    {
+      trip: 'Paris',
+      user: 'John Doe',
+      date: '2021-01-01',
+      status: 'Cancelled',
+      price: 100
+    }
+  ];
+
+  const recentBookingsRef = useRef(null);
+  const tableRef = useRef(null);
+
+  useEffect(() => {
+    const recentBookingsHeight = recentBookingsRef.current.clientHeight;
+    const headingHeight = recentBookingsRef.current.querySelector('h3').clientHeight;
+    tableRef.current.style.height = `${recentBookingsHeight - headingHeight}px`;
+  }, [recentBookingsRef]);
+
+  return (
+    <div ref={recentBookingsRef} className="h-100">
+      <h3 className="mb-3">Recent Booking Activity</h3>
+      <div ref={tableRef} className="hidescroll" style={{ overflow: 'scroll' }}>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Trip</th>
+              <th scope="col">User</th>
+              <th scope="col">Date</th>
+              <th scope="col">Status</th>
+              <th scope="col">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking, index) => (
+              <tr key={index} onClick={() => console.log('clicked')} style={{ cursor: 'pointer' }}>
+                <td>{booking.trip}</td>
+                <td>{booking.user}</td>
+                <td>{booking.date}</td>
+                <td className="text-center">
+                  {booking.status === 'Paid' && <span className="alert alert-success p-1 rounded w-100">{booking.status}</span>}
+                  {booking.status === 'Pending' && <span className="alert alert-warning p-1 rounded w-100">{booking.status}</span>}
+                  {booking.status === 'Cancelled' && <span className="alert alert-danger p-1 rounded w-100">{booking.status}</span>}
+                </td>
+                <td className="text-success">${booking.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default RecentBookings;
--- web/src/views/dashboard/Main.tsx
+++ web/src/views/dashboard/Main.tsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from 'react';
+
 import { Card } from '@mui/material';
 import { People, Visibility, Tour, Apartment, Bookmark } from '@mui/icons-material';
 
@@ -5,12 +7,20 @@ import DashboardExpenses from '@/components/dashboard/ExpensesChart';
 import DashboardVisits from '@/components/dashboard/VisitsChart';
 import DashboardStatCard from '@/components/dashboard/StatCard';
 import DashboardTopSavedTrips from '@/components/dashboard/TopSavedTrips';
+import DashboardRecentBookings from '@/components/dashboard/RecentBookings';
 
 const Main = () => {
+  const expensesRef = useRef(null);
+
+  useEffect(() => {
+    const expensesHeight = expensesRef.current.clientHeight;
+    const recentBookingsElement = document.querySelector('.recent-bookings');
+    recentBookingsElement.style.height = `${expensesHeight}px`;
+  }, []);
 
   return (
     <div className="container-fluid">
-      <div className="row g-3">
+      <div className="row hidescroll rounded-3 g-3" style={{ height: '90vh', overflow: 'scroll' }}>
         <div className="col-2">
           <DashboardStatCard value={27} name="Trips" comment="in total" Icon={Tour} iconColor="red"  />
         </div>
@@ -29,24 +39,24 @@ const Main = () => {
         <div className="col-2">
           <DashboardStatCard value={3} name="Offices" comment="in total" Icon={Apartment} iconColor="blue" />
         </div>
-        <div className="col-12 col-md-6 col-lg-6">
-          <Card className="p-3 bg-white text-dark" elevation={0}>
+        <div className="col-12 col-md-6 col-lg-5">
+          <Card className="p-3 bg-white text-dark" elevation={0} ref={expensesRef}>
             <DashboardExpenses />
+          </Card>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <Card className="p-3 bg-white text-dark recent-bookings" elevation={0}>
+            <DashboardRecentBookings />
+          </Card>
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <Card className="p-3 bg-white text-dark h-100" elevation={0}>
+            <DashboardTopSavedTrips />
           </Card>
         </div>
         <div className="col-12 col-md-6 col-lg-6">
           <Card className="p-3 bg-white text-dark h-100" elevation={0}>
             <DashboardVisits />
-          </Card>
-        </div>
-        <div className="col-12 col-md-6 col-lg-4">
-          <Card className="p-3 bg-white text-dark h-100" elevation={0}>
-            <DashboardTopSavedTrips />
-          </Card>
-        </div>
-        <div className="col-12 col-md-6 col-lg-4">
-          <Card className="p-3 bg-white text-dark h-100" elevation={0}>
-            <DashboardTopSavedTrips />
           </Card>
         </div>
         <div className="col-12 col-md-6 col-lg-4">
